f5aebc4f6fc5629207bf66cd9fba95350fd46748
AlexNiunko <alexniunko89@gmail.com>
2024-04-13 20:48:48 +0300

    13.04.2024

--- src/main/java/com/aston_rest_api/model/Sale.java
+++ src/main/java/com/aston_rest_api/model/Sale.java
@@ -8,7 +8,6 @@ public class Sale extends AbstractEntity {
     private long buyerId;
     private long productId;
     private LocalDate dateOfSale;
-
     private Product productOfSale;
     private int amountSale;
 
@@ -19,71 +18,6 @@ public class Sale extends AbstractEntity {
         this.dateOfSale=builder.dateOfSale;
         this.amountSale= builder.amountSale;
         this.productOfSale= builder.productOfSale;
-    }
-
-    public long getBuyerId() {
-        return buyerId;
-    }
-
-    public void setBuyerId(long buyerId) {
-        this.buyerId = buyerId;
-    }
-
-    public long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(long productId) {
-        this.productId = productId;
-    }
-
-    public LocalDate getDateOfSale() {
-        return dateOfSale;
-    }
-
-    public void setDateOfSale(LocalDate dateOfSale) {
-        this.dateOfSale = dateOfSale;
-    }
-
-    public int getAmountSale() {
-        return amountSale;
-    }
-
-    public void setAmountSale(int amountSale) {
-        this.amountSale = amountSale;
-    }
-
-    public Product getProductOfSale() {
-        return productOfSale;
-    }
-
-    public void setProductOfSale(Product productOfSale) {
-        this.productOfSale = productOfSale;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Sale)) return false;
-        Sale sale = (Sale) o;
-        return getBuyerId() == sale.getBuyerId() && getProductId() == sale.getProductId() && getAmountSale() == sale.getAmountSale() && getDateOfSale().equals(sale.getDateOfSale());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getBuyerId(), getProductId(), getDateOfSale(), getAmountSale());
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Sale{");
-        sb.append("buyerId=").append(buyerId);
-        sb.append(", productId=").append(productId);
-        sb.append(", dateOfSale=").append(dateOfSale);
-        sb.append(", amountSale=").append(amountSale);
-        sb.append(", id=").append(id);
-        sb.append('}');
-        return sb.toString();
     }
 
     public static class SaleBuilder {
